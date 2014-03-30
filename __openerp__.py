7d47ce28b8950f88667aa03a511fb17bead9e3ab
Niels Ruelens <niels.ruelens@clubit.be>
2014-03-30 11:52:00 +0200

    first version, added new partner reference field

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'clubit_expertm',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': "Expert/M EDI integration",
+    'author': 'Niels Ruelens',
+    'website': 'http://clubit.be',
+    'summary': "Creates an EDI integration with Expert/M software",
+    'sequence': 9,
+    'depends': [
+        'account',
+        'clubit_tools',
+    ],
+    'data': [
+        'partner.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ partner.py
@@ -0,0 +1,9 @@
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
+class res_partner(osv.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+    _columns = {
+        'reference': fields.char('Reference', size=64),
+    }
