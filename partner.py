e7bae4ee2a1b453226f4434b27a82db954ccee8d
Niels Ruelens <niels.ruelens@clubit.be>
2014-03-30 13:33:39 +0200

    added invoice account payable/receivable

--- partner.py
+++ partner.py
@@ -6,4 +6,20 @@ class res_partner(osv.Model):
     _inherit = "res.partner"
     _columns = {
         'reference': fields.char('Reference', size=64),
+        'invoice_account_payable': fields.property(
+            'account.account',
+            type='many2one',
+            relation='account.account',
+            string="Invoice Account Payable",
+            view_load=True,
+            domain="[('type', '=', 'payable')]",
+            help="This account is used during invoice line account determination."),
+        'invoice_account_receivable': fields.property(
+            'account.account',
+            type='many2one',
+            relation='account.account',
+            string="Invoice Account Receivable",
+            view_load=True,
+            domain="[('type', '=', 'receivable')]",
+            help="This account is used during invoice line account determination."),
     }
